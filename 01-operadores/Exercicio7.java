7952608ad8e9b13d11b168190d8cd1775d8636be
Victoria Riso Santana <devvicrisosan@gmail.com>
2026-03-25 10:01:03 -0300

    docs(operadores): adiciona descrição exercício 7 - categoria atletas por idade

---
+++ 01-operadores/Exercicio7.java
@@ -0,0 +1,14 @@
+/**
+ * Exercício 7: Determinar a Categoria de um Atleta com Base na Idade
+ *
+ * Suponha que você queira determinar a categoria de um atleta com base em sua idade:
+ * - Se a idade for inferior a 18, a categoria é "Juvenil".
+ * - Se a idade estiver entre 18 e 40 (inclusive), a categoria é "Adulto".
+ * - Se a idade for superior a 40, a categoria é "Master".
+ * Usar apenas o operador ternário.
+ */
+
+public class Exercicio7 {
+    public static void main(String[] args) {
+    }
+}
