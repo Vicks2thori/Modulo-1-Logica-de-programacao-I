3d3072ad28f3d223c9b9abe7ff34e7e1a09b5e4c
Victoria Riso Santana <devvicrisosan@gmail.com>
2026-03-24 22:03:53 -0300

    docs(operadores): adiciona descrição exercício 6 - maior de três números

---
+++ 01-operadores/Exercicio6.java
@@ -0,0 +1,11 @@
+/**
+ * Exercício 6: Verificar o Maior de Três Números
+ *
+ * Escreva um programa que aceite três números inteiros e determine o maior entre eles
+ * usando apenas o operador ternário.
+ */
+
+public class Exercicio6 {
+    public static void main(String[] args) {
+    }
+}
