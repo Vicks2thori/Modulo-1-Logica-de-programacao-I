006857d679373f3a71b8cef807e39f42eafdbd62
Victoria Riso Santana <devvicrisosan@gmail.com>
2026-03-25 20:34:03 -0300

    feat(try-catch-ex2): concluido Exercicio 2 - Try e Catch Bruna, Filipe, Tarsilla Thank You!

--- 11-try-catch/Exercicio2.java
+++ 11-try-catch/Exercicio2.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Exercício 2: Scanner, try-catch-finally método
  *
@@ -7,5 +9,19 @@
 
 public class Exercicio2 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um numero ou uma frase: ");
+        String string = sc.next();
+        Integer number;
+
+        try {
+            number = Integer.valueOf(string);
+            System.out.printf("O seu numero %d é um numero inteiro válido", number);
+        } catch (NumberFormatException e) {
+            System.err.println("O seu numero não é um numero inteiro válido");
+        } finally {
+            System.out.println("Encerrando o programa!");
+            sc.close();
+        }
     }
 }
