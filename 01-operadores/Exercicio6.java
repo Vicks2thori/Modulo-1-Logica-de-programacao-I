748e2e577167cd8570090fe6228c53917861ba03
Victoria Riso Santana <devvicrisosan@gmail.com>
2026-03-24 22:59:41 -0300

    feat(operadores): resolve exercício 6 - maior de três números

--- 01-operadores/Exercicio6.java
+++ 01-operadores/Exercicio6.java
@@ -7,5 +7,15 @@
 
 public class Exercicio6 {
     public static void main(String[] args) {
+        int numero1 = 30;
+        int numero2 = 15;
+        int numero3 =20;
+
+        int maiorTresNumeros =
+                numero1 > numero2 && numero1 > numero3
+                ? numero1 : numero2 > numero3
+                ? numero2 : numero3;
+
+        System.out.println(maiorTresNumeros);
     }
 }
