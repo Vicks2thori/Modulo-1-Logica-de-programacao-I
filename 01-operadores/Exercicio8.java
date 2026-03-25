92d6f5c1b746539fa2feb7a441246aee99114df2
Victoria Riso Santana <devvicrisosan@gmail.com>
2026-03-25 10:15:55 -0300

    docs(operadores-ex8): adiciona descrição Exercício 8 - Ano Bissexto

---
+++ 01-operadores/Exercicio8.java
@@ -0,0 +1,13 @@
+/**
+ * Exercício 8: Verificar se um Ano é Bissexto
+ *
+ * Um ano é considerado bissexto se:
+ * - É divisível por 4, mas não por 100, ou
+ * - É divisível por 400.
+ * Escreva um programa que determine se um ano é bissexto ou não, usando apenas o operador ternário.
+ */
+
+public class Exercicio8 {
+    public static void main(String[] args) {
+    }
+}
